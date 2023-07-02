8835b34fc4153ff5662b90b711a7b00a500907d1
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-07-02 03:02:24 -0300

    refactor: :fire: Delete default stories of storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,8 @@
-import { colors } from "@igniteCall-ui/tokens";
+import { styled } from "./styles";
 
-export function App() {
-    return <h1 style={{ color: colors.ignite500 }}>Hello Mundo</h1>;
-}
+export const Button = styled("button", {
+    fontFamily: "$default",
+    backgroundColor: "$ignite500",
+    borderRadius: "$md",
+    padding: "$4",
+});
