28862d0901f331f2cf8ddbc4f01b6dd274be49de
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-07-05 23:00:24 -0300

    feat: :sparkles: Add CheckBox component story

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,7 +1,11 @@
 export * from "./components/Avatar";
+
 export * from "./components/Box";
+
 export * from "./components/Button";
-export * from "./components/Heading";
+export * from "./components/CheckBox";
 export * from "./components/TexInput";
-export * from "./components/Text";
 export * from "./components/TextArea";
+
+export * from "./components/Heading";
+export * from "./components/Text";
