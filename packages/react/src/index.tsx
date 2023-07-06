0cccd628d9d757b242074cfe9be214633f954c73
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-07-05 23:24:17 -0300

    feat: :sparkles: Add MultiForm component story

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -4,6 +4,7 @@ export * from "./components/Box";
 
 export * from "./components/Button";
 export * from "./components/CheckBox";
+export * from "./components/MultiStep";
 export * from "./components/TexInput";
 export * from "./components/TextArea";
 
